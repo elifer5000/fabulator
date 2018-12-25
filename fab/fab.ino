61c8be633b8fc56679ce889b69fc3db183c80d12
Elias Cohenca <elias.cohenca@autodesk.com>
2018-12-25 16:04:21 +0200

    Arduino stuff

--- fab/fab.ino
+++ fab/fab.ino
@@ -9,6 +9,14 @@ Stepper steppers[NUM_STEPPERS] = {
   Stepper(Q_STEP_PIN, Q_DIR_PIN, Q_ENABLE_PIN, Q_MS1_PIN, Q_MS2_PIN, Q_MS3_PIN)
 };
 
+// create a random integer from 0 - bound
+unsigned int rng(int bound) {
+  static unsigned int y = 0;
+  y += micros(); // seeded with changing number
+  y ^= y << 2; y ^= y >> 7; y ^= y << 7;
+  return (y / 65535.0f) * bound;
+}
+
 byte i;
 
 void setupSteppers() {
@@ -17,12 +25,31 @@ void setupSteppers() {
     }
 }
 
+unsigned long start = 0, current = 0, total = 0;
+
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(500000);
     setupSteppers();
+    start = millis();
+
+//    for (i = 0; i < NUM_STEPPERS; i++) {
+//        steppers[i].setNote(440, 5);
+//      }
 }
 
+int f = 0, s = 1;
 void loop() {
+    current = millis();
+
+//    if (current - start > 10) {
+//      for (i = 0; i < NUM_STEPPERS; i++) {
+//        if (f > 800) s = -1;
+//        if (f < 0) s = 1;
+//        f += s;
+//        steppers[i].setNote(440 + f, 5);
+//      }
+//      start = current;
+//    }
     handleSerial();
 
     for (i = 0; i < NUM_STEPPERS; i++) {
